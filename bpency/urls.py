845080a2c42eaaf17aeea8cb4b031acd5eadb7ad
namzoen <bhoowakuls@gmail.com>
2021-05-21 20:36:28 +0700

    laman konversi and top nav-bar (unfinished) added.

--- bpency/urls.py
+++ bpency/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # ex: / or empty
     path('', views.index, name='index'),
     # ex: /login
-    # path('/login', views.login, name='login'),
-    # ex: /login
-    # path('/signup', views.signup, name='signup'),
+    path('login', views.nav, name='login'),
+    # ex: /konversi
+    path('konversi', views.konversi, name='konversi'),
 ]
--- bpency/views.py
+++ bpency/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'bpency/index.html')
+
+def nav(request):
+    return render(request, 'bpency/nav.html')
+
+def konversi(request):
+    return render(request, 'bpency/konversi.html')
