75b40284db8bed312c26a98be424d3f4db156492
paijolongor <zidankarim5@gmail.com>
2021-05-26 11:58:07 +0700

    Revert "set up database mysql"
    This reverts commit cfb62fb5ded8da39909bd56b9007f72ba80090bf.

--- bpence/settings.py
+++ bpence/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'bpence.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bpence',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
