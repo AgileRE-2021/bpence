f4f3d7fdc91954682a8a719f17994a514aa97a98
paijolongor <zidankarim5@gmail.com>
2021-05-26 11:57:44 +0700

    Revert "add tables"
    This reverts commit b69730ad3d0d61ca6c77809fceb3ceffacd5479d.

--- bpency/models.py
+++ bpency/models.py
@@ -1,9 +1,3 @@
 from django.db import models
-from django.db.models.base import Model
 
 # Create your models here.
-class SD(models.Model):
-    code = models.TextField()
-
-class BPMN(models.Model):
-    api = models.TextField()
