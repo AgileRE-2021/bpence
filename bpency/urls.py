a14ae979d62276e3ce6e4ff6b8770939d2ee3c4e
namzoen <bhoowakuls@gmail.com>
2021-06-02 23:40:21 +0700

    navbar added, blank login and signup page added

--- bpency/urls.py
+++ bpency/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     # ex: / or empty
     path('', views.index, name='index'),
     # ex: /login
-    path('login', views.nav, name='login'),
+    path('login', views.login_view, name='login'),
+    # ex: /signup
+    path('signup', views.signup_view, name='signup'),
     # ex: /konversi
     path('konversi', views.konversi, name='konversi'),
 ]
--- bpency/views.py
+++ bpency/views.py
@@ -6,11 +6,13 @@ from .models import SD
 def index(request):
     return render(request, 'bpency/index.html')
 
-def nav(request):
-    return render(request, 'bpency/nav.html')
+def login_view(request):
+    return render(request, 'bpency/login.html')
+
+def signup_view(request):
+    return render(request, 'bpency/signup.html')
 
 def konversi(request):
-    
     if request.method == 'POST':
         sd = SD(code=request.POST["codeplant"])
         sd.save()
